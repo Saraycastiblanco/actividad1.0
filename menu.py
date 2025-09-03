ca52333c580d053476dc0debf5e0a8764adf3490
karendanielapoveda1608@gmail.com <juli.rivera.quintero@gmail.com>
2025-09-03 13:44:44 -0500

    suma y resta

--- menu.py
+++ menu.py
@@ -6,3 +6,8 @@ num2 = float(input("Introduce el segundo número: "))
 print("Menú de operaciones: suma, resta, multiplicación, división, raíz, porcentaje")
 operador = input("Elige una operación: ")
 
+if operador == "suma":
+    resultado = num1 + num2
+
+elif operador == "resta":
+    resultado = num1 - num2
