22baebf2a2ce79ea914646d1cefad5f8a8ef6138
karendanielapoveda1608@gmail.com <juli.rivera.quintero@gmail.com>
2025-09-03 13:08:28 -0500

    plantilla

---
+++ menu.py
@@ -0,0 +1,7 @@
+import math
+num1 = float(input("Introduce el primer número: "))
+num2 = float(input("Introduce el segundo número: "))
+
+print("Menú de operaciones: suma, resta, multiplicación, división, raíz, porcentaje")
+operador = input("Elige una operación: ")
+
